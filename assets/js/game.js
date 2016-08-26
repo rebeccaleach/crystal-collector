d63cb62254dbc1f01a23f80264f27beb41454440
Rebecca Leach <flytrapcat@gmail.com>
2016-08-26 15:11:36 -0500

    finished, i think

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,14 +2,95 @@
 
 $(document).ready(function(){
 
-	$('#finn').on('click', function() {
-			$('#finn').html('');
-			$('#you').html(
-        		'<div class="wrap" id="finn"><p>Finn</p><img src="assets/images/finn.jpg" alt="Finn"><p>120</p></div>'
-				);
-			$('#you').css('background-color', 'green');
+	var score = 0;
 
+	var wins = 0;
+
+	var losses = 0;
+
+	var randomNumber = Math.round(Math.random() * 100) + 10;
+	console.log('random number: ' + randomNumber);
+
+	var greenNumber = Math.round(Math.random() * 20);
+
+	var redNumber = Math.round(Math.random() * 20);
+
+	var blueNumber = Math.round(Math.random() * 20);
+
+	var purpleNumber = Math.round(Math.random() * 20);
+
+
+	function reset() {
+		score = 0;
+		
+		randomNumber = Math.round(Math.random() * 100) + 10;
+		console.log('new random number: ' + randomNumber);
+		$('#randomNumber').html(randomNumber);
+
+		greenNumber = Math.round(Math.random() * 20);
+
+		redNumber = Math.round(Math.random() * 20);
+
+		blueNumber = Math.round(Math.random() * 20);
+
+		purpleNumber = Math.round(Math.random() * 20);
+	}
+
+
+
+	function message() {
+		if (randomNumber === score) {
+			$('#message').html('<h1>You win!</h1>');
+			wins += 1;
+			$('#wins').html(wins);
+			reset();
+		}
+
+		else if (randomNumber < score) {
+			$('#message').html('<h1>You lose</h1>');
+			losses += 1;
+			$('#losses').html(losses);
+			reset();
+
+		}
+
+		else {
+			$('#message').html('<h1>Keep clicking....</h1>')
+		}
+	}
+
+
+
+	$('#randomNumber').html(randomNumber);
+
+	$('#green').on('click', function() {
+		console.log('green: ' + greenNumber);
+		score += greenNumber;
+		$('#score').html(score);
+		message();
 	});
+
+	$('#red').on('click', function() {
+		console.log('red: ' + redNumber);
+		score += redNumber;
+		$('#score').html(score);
+		message();
+	});
+
+	$('#blue').on('click', function() {
+		console.log('blue: ' + blueNumber);
+		score += blueNumber;
+		$('#score').html(score);
+		message();
+	});
+
+	$('#purple').on('click', function() {
+		console.log('purple: ' + purpleNumber);
+		score += purpleNumber;
+		$('#score').html(score);
+		message();
+	});
+
 
 
 
